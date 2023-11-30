b2f984a24542d42d91237dd7982c36e6031b49ec
Purushottam Sonar <sonarp02@gmail.com>
2023-11-30 16:59:20 +0530

    Navigation section finished

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,8 +1,36 @@
 import React from "react";
-
 import "./Nav.css";
+import { FiHeart } from "react-icons/fi";
+
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineUserAdd } from "react-icons/ai";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <>
+      <nav>
+        <div className="nav-container">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Enter your search shoes."
+          />
+        </div>
+
+        <div className="profile-conatiner">
+          <a href="#">
+            <FiHeart className="nav-icons" />
+          </a>
+          <a href="#">
+            <AiOutlineShoppingCart className="nav-icons" />
+          </a>
+          <a href="#">
+            <AiOutlineUserAdd className="nav-icons" />
+          </a>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
