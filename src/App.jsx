abd6ee3fcb37bba8579dc84fc14f2178dd89bf7a
Purushottam Sonar <sonarp02@gmail.com>
2023-11-30 15:39:39 +0530

    Folder structure is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Nav from "./navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
 
 const App = () => {
   return (
     <>
-      <h1>Shubham</h1>
+      <Nav />
+      <Products />
+      <Recommended />
     </>
   );
 };
---
+++ src/Recommended/Recommended.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Recommended.css";
+const Recommended = () => {
+  return <div>Recommended</div>;
+};
+
+export default Recommended;
